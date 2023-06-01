dc108264777fafa73366fa26973922c01fbd9248
Vihangamethsara1 <120578494+Vihangamethsara1@users.noreply.github.com>
2023-06-01 16:59:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,27 +36,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.vcardowner = ['94761105459'] //ur owner number
+global.ownername = "𝑽𝒊𝒉𝒂𝒏𝒈𝒂_𝑴𝒆𝒕𝒉𝒔𝒂𝒓𝒂 ( © ᴹᵀ 𝑫𝒂𝒓𝒌_𝑫𝒆𝒗𝒐𝒍𝒐𝒑𝒆𝒓" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.location = "Sri Lanka, Wellawaya, Buduruwagala" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.owner = ['94761105459']
+global.ownertag = '94761105459' //ur tag number
+global.botnumber = '1(315)889-6502' //Add your Bot Number For Log
+global.botname = 'CYBER-X BETA ' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
+global.reactmoji = "🌀" //ur menu react emoji
+global.themeemoji = "🖤" //ur theme emoji
+global.packname = "Cyber-X  Bot" //ur sticker watermark packname
+global.author = "DARK Maker" //ur sticker watermark author
+global.footer = "® 𝑪𝒚𝒃𝒆𝒓 - 𝑿 𝑩𝒐𝒕 𝑫𝒆𝒑𝒍𝒐𝒚 𝑩𝒚 𝑫𝒂𝒓𝒌 𝑴𝒂𝒌𝒆𝒓"
+global.aliveimg ="https://telegra.ph/file/e7df8d458a85466cdc785.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
 global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
